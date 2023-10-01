50947afb31c9f8caa5befebf23fad777d211f57e
Rohankupate <kupaterohan@gmail.com>
2023-10-01 18:40:55 +0530

    some added

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
 app.get("/" , (req , res)=>{
-    res.json({msg : "welcome to backend"})
+    res.json({msg : "welcome to backend,i am here"})
 })
 
 try {
