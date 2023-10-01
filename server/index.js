307313284807c788a9d613a41c7b24c3091568a5
Rohankupate <kupaterohan@gmail.com>
2023-10-01 12:53:04 +0530

    added known_hosts

--- server/index.js
+++ server/index.js
@@ -15,6 +15,20 @@ app.use("/user", userRouter)
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+// mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+try {
+    mongoose.connect('mongodb+srv://admin:ctrl4545@cluster0.0tn1hlx.mongodb.net/Courses', {
+        
+        dbName: "Courses"
+    });
+    console.log("Connected to MongoDB successfully!");
+    // You can perform operations with the database here
+} catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    // Handle the error (e.g., retry the connection, terminate the application, etc.)
+}
+
+
+// mongodb+srv://admin:ctrl4545@cluster0.0tn1hlx.mongodb.net/Courses
 
 app.listen(3000, () => console.log('Server running on port 3000'));
