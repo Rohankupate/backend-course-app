36a1f2f3d14aa833c0960c8cd2f0fb69a7455b71
Rohankupate <kupaterohan@gmail.com>
2023-10-03 11:04:08 +0530

    host added

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "todo-app-class.pem" ubuntu@ec2-51-20-103-219.eu-north-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-class.pem" ubuntu@ec2-16-171-232-220.eu-north-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
